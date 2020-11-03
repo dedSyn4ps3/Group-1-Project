c718ad55544a831900cbe35997e2b328b6d7f84a
Ed Rutherford <erutherford@nullsecurity.tech>
2020-11-03 11:51:10 -0500

    Initial Commit

---
+++ Group 1 Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Group 1 Project
+//
+//  Created by Eddie Sneed on 11/3/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
