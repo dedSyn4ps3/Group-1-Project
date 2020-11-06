bb01fa2239c44320f8bf67ad5c8fc40c361a1878
shard208 <73910194+shard208@users.noreply.github.com>
2020-11-06 16:50:29 -0600

    Add files via upload

---
+++ phoneprices.c
@@ -0,0 +1,34 @@
+// James Finch
+//11/6/2020
+//Phone Comparison
+
+
+
+void displayComparison(){
+//declare variables
+string Model A, Model B, Model C;
+string Plan A, Plan B, Plan C;
+
+//our phone prices
+cout << "Our Prices" << endl;
+cout << "Model A: $99.00" << endl;
+cout << "Model B: $399.00" << endl;
+cout << "Model C: $799.00" << endl;
+// competitors prices
+cout << "Competitors Prices" << endl;
+cout << "Model A: $149.00" << endl;
+cout << "Model B: $599.00" << endl;
+cout << "Model C: $875.00" << endl;
+
+//Plan prices
+//our phone prices
+cout << "Our Plans" << endl;
+cout << "Plan A: $25.00" <<endl;
+cout << "Plan B: $35.00" << endl;
+cout << "Plan C: $50.00" << endl;
+// competitors prices
+cout << "Competitors Plans" << endl;
+cout << "Plan A: $36.99" << endl;
+cout << "Plan B: $69.99" << endl;
+cout << "Plan C: $79.99" << endl;
+}
