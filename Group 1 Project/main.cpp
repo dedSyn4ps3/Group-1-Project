456f3766f52ca549d3960d27e8b87ce4f2f59da4
EddieSneed <x00syn4ps3@outlook.com>
2020-11-07 21:23:26 -0500

    Update main.cpp
    Finished up the rest of 'main' for our group's program

--- Group 1 Project/main.cpp
+++ Group 1 Project/main.cpp
@@ -2,13 +2,181 @@
 //  main.cpp
 //  Group 1 Project
 //
-//  Created by Eddie Sneed on 11/3/20.
-//
+//  Created by Ed Rutherford on 11/03/20
+//  Edited on 11/07/2020
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include "Phone.h"
+#include "Plan.h"
+
+using namespace std;
+
+const string phoneFile = "phoneList.csv";
+const string planFile = "planList.csv";
+
+bool openFile(fstream& file, string name)
+{
+   file.open(name, ios::in);
+   if (file.fail())
+      return false;
+   else
+      return true;
+}
+
+void createPhoneRecord() {
+
+    // file pointer
+    fstream file;
+
+    // creates a new file
+    file.open(phoneFile, ios::out | ios::app);
+
+    string name, address, city, state, zip;
+
+    cout << "[+] Please provide the following information to update new Phone records >>>";
+
+    cout << "\nEnter Model Name: ";
+    cin.ignore();
+    getline(cin, name);
+    file << name << ",";
+    
+    cout << "Enter Release Year: ";
+    getline(cin, address);
+    file << address << ",";
+    
+    cout << "Enter Price: ";
+    getline(cin, city);
+    file << city << endl;
+
+    file.close();
+}
+
+void readPhoneRecord() {
+
+    fstream file;
+    
+    if (openFile(file, phoneFile))
+    {
+       showContent(file);
+       file.close();
+    }
+    else
+       cout << "[!] File Open Error!" << endl;
+}
+
+void createPlanRecord() {
+
+    // file pointer
+    fstream file;
+
+    // creates a new file
+    file.open(planFile, ios::out | ios::app);
+
+    string name, address, city, state, zip;
+
+    cout << "[+] Please provide the following information to update new Plan records >>>";
+
+    cout << "\nEnter Plan Name: ";
+    cin.ignore();
+    getline(cin, name);
+    file << name << ",";
+    
+    cout << "Enter Price: ";
+    getline(cin, city);
+    file << city << endl;
+
+    file.close();
+}
+
+void readPlanRecord() {
+
+    fstream file;
+    
+    if (openFile(file, planFile))
+    {
+       showContent(file);
+       file.close();
+    }
+    else
+       cout << "[!] File Open Error!" << endl;
+}
+
+void showContent(fstream& file)
+{
+   string line;
+    
+   while (getline(file, line)) //file >> line
+   {
+     cout << line << endl;
+       
+   }
+}
+
+void displayComparison()
+{
+   //Our phone prices
+   cout << "[+] Our Prices >>> \n" << endl;
+   readPhoneRecord();
+
+   //Competitors prices
+   cout << "\n[-] Competitors Prices >>> " << endl;
+   cout << "\nModel A: $149.00" << endl;
+   cout << "Model B: $599.00" << endl;
+   cout << "Model C: $875.00" << endl;
+
+   //Plan prices
+   //Our plan prices
+   cout << "[+] Our Plans >>> \n" << endl;
+   readPlanRecord();
+
+   //Competitors prices
+   cout << "\n[-] Competitors Plans >>> " << endl;
+   cout << "\nPlan A: $36.99" << endl;
+   cout << "Plan B: $69.99" << endl;
+   cout << "Plan C: $79.99" << endl;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int choice;
+
+    int* menuPtr = &choice;
+
+    *menuPtr = 0;
+
+    do
+    {
+        cout << "====================================================" << endl;
+        cout << "\n[+] Group 1 | Phones & Plans Console Program | >>>";
+        cout << "\n[Please select from the following choices]: \n";
+
+        cout << "1. Enter New Phone Model Details (Admins ONLY)\n";
+        cout << "2. Enter New Plan Details (Admins ONLY)\n";
+        cout << "3. View Phone & Plan Prices vs Our Competitors\n";
+        cout << "4. Exit Program\n\n";
+        cout << "====================================================" << endl;
+
+        cin >> *menuPtr;
+
+        switch (choice) {
+        case 1:
+            createPhoneRecord();
+            break;
+        case 2:
+            createPlanRecord();
+            break;
+         case 3:
+            displayComparison();
+            break;
+        case 4:
+            break;
+
+        default:
+            cout << "[!] Please make valid selection...";
+        }
+    } while (*menuPtr != 4);
+
     return 0;
 }
